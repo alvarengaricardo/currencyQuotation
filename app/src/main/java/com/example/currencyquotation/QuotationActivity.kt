2d64b73a09e7b534b6e8662bdbc3efff44f8910d
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-01-10 00:44:08 -0300

    screens

--- app/src/main/java/com/example/currencyquotation/QuotationActivity.kt
+++ app/src/main/java/com/example/currencyquotation/QuotationActivity.kt
@@ -1,4 +1,10 @@
 package com.example.currencyquotation
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class QuotationActivity {
+class QuotationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotation)
+    }
 }
