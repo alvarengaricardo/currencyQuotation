c10fa4cd75763231fdd43414fec5e1056080dde7
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-01-11 22:11:27 -0300

    restart

---
+++ app/src/main/java/com/example/currencyquotation/Currency.kt
@@ -0,0 +1,5 @@
+package com.example.currencyquotation
+
+class Currency(
+    var USD: Double
+)
---
+++ app/src/main/java/com/example/currencyquotation/Request.kt
@@ -0,0 +1,6 @@
+package com.example.currencyquotation
+
+class Request(
+    var date: String,
+    var result: Currency
+)
